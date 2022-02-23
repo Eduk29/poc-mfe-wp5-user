a2a0c332ea8e5da8c957eb9fadeb7749f3cc970e
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-02-23 17:43:07 -0300

    feat: Integrated User to Shell

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,9 +4,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
+
+if (!(window as any).shell && environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+declare const require: any;
+		const ngVersion = require('../package.json').dependencies['@angular/core'];
+
+		(window as any).plattform = (window as any).plattform || {};
+		let platform = (window as any).plattform[ngVersion];
+		if (!platform) {
+		  platform = platformBrowserDynamic();
+		  (window as any).plattform[ngVersion] = platform;
+		}
+		platform.bootstrapModule(AppModule)
+		  .catch(err => console.error(err));
